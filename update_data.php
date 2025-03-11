51b9bdcd1055ef0c0684a608765db7cb6b3b50da
bhavithran2007 <106386134+bhavithran2007@users.noreply.github.com>
2025-03-12 04:38:15 +0530

    Delete update_data.php

--- update_data.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_GET['temp']) && isset($_GET['humidity']) && isset($_GET['soil']) && isset($_GET['motion'])) {
-    $data = $_GET['temp'] . "," . $_GET['humidity'] . "," . $_GET['soil'] . "," . $_GET['motion'];
-    file_put_contents("sensor_data.txt", $data);
-    echo "Data updated";
-}
-?>
